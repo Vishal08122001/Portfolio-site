cafa58491a97c53aeb90b3bc94ecf618a0813213
Vishal08122001 <vc31142@gmail.com>
2023-02-05 19:02:11 +0530

    ffhg

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -32,7 +32,7 @@ const Portfolio = () => {
             className="bg-gradient-to-b from-black to-gray-800 w-full md:h-screen text-white"
         >
             <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-                <div className="pb-8" style={{ paddingTop: '10rem' }}>
+                <div className="pb-8" style={{ paddingTop: '7rem' }}>
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">
                         Portfolio
                     </p>
