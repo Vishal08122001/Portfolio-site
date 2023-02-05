cf21247d16e994a4d4f51634a3ac8034b878f048
Vishal08122001 <vc31142@gmail.com>
2023-02-05 19:05:26 +0530

    ds

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,9 +14,13 @@ const About = () => {
         </div>
         <p className="text-xl mt-20">
           As you know my name is Vshal. I am basically from Mathura(Uttar Pradesh) but, i am settled in Faridabad from my childhood. Talking about my educational qualifications i completed my SSC from Goverment Modal Senior Secondary School with 8.4 CGPA
-          <br />
+
           Currently i am persuring Bachelor in Commerce from University of Delhi (SOL) apart from that i am doing Web Development from Almabetter (Bangalore) in which till now i have completed HTML, CSS, Javascript React, Redux, Node.js, Express.js, Mongoose, MongoDB, SQL etc.
-          <br />
+        </p>
+
+        <br />
+
+        <p>
           My short-term goal is to get a job in a reputed company so that I can implement as well as improve my knowledge and skill.
           My long-term goal is to achieve a good position where. I can build my carrier and help the organization.
         </p>
